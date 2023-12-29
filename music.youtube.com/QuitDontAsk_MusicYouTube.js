b666a533da825b7bf70f3d51f433c2f6e22439a3
sanhphanvan96 <28304961+sanhphanvan96@users.noreply.github.com>
2023-12-29 09:34:47 +0700

    Create QuitDontAsk_MusicYouTube.js

---
+++ music.youtube.com/QuitDontAsk_MusicYouTube.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        QuitDontAsk - music.youtube.com
+// @namespace   Violentmonkey Scripts
+// @match       https://music.youtube.com/
+// @grant       none
+// @version     1.0
+// @author      sphan
+// @description 12/12/2023, 6:13:37 PM
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    window.addEventListener('beforeunload', function (e) {
+      e.stopImmediatePropagation();
+    }, true);
+})();
